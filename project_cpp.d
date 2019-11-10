8adc0e7a28075a53a3f7e87205e34e7134eca8ab
WishIWasBornInTheCreteaceousEra <nat15dil@student.lu.se>
2019-11-10 15:51:42 +0100

    Adding HotCold plot and Gaussian, still some issues with output.

--- project_cpp.d
+++ project_cpp.d
@@ -2,5 +2,5 @@
 # DO NOT DELETE
 
 ./project_cpp.so: tempTrender.h
-./project_cpp.so: /home/dolev/root/include/RVersion.h /home/dolev/root/include/RConfig.h /home/dolev/root/include/TClass.h /home/dolev/root/include/TDictAttributeMap.h /home/dolev/root/include/TInterpreter.h /home/dolev/root/include/TROOT.h /home/dolev/root/include/TBuffer.h /home/dolev/root/include/TMemberInspector.h /home/dolev/root/include/TError.h /home/dolev/root/include/RtypesImp.h /home/dolev/root/include/TIsAProxy.h /home/dolev/root/include/TFileMergeInfo.h /home/dolev/root/include/TCollectionProxyInfo.h /home/dolev/root/bin/rootcling
-project_cpp__ROOTBUILDVERSION= 6.18/04
+./project_cpp.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+project_cpp__ROOTBUILDVERSION= 5.34/30
--- tempTrender.cpp
+++ tempTrender.cpp
@@ -12,6 +12,7 @@
 #include <TMath.h>   // math functions
 #include <TCanvas.h> // canvas object
 #include <THStack.h>
+#include <TLegend.h>
 #include "tempTrender.h"
 
 using namespace std;
@@ -19,43 +20,36 @@ using namespace std;
 
 tempTrender::tempTrender(string filePath){
 	cout << "The user supplied " << filePath << " as the path to the data file." << endl;
-	FilePath = filePath;
 	
-	//Extract File Name & set it to global variable FileName.
+	//Creating File Name to use in saving of figures.
 	int pos1 = filePath.find("/"); 
 	string fileName = filePath.substr(pos1 + 1);
 	int pos2 = fileName.find(".");
 	fileName = fileName.substr(0,  pos2);
-	
 	FileName=fileName;
+	//Creating Data Set.
+	ifstream f(filePath.c_str());
+	if (f.fail()){
+		cerr<<"Could not open file.\n";
+	}
+	Reader(f, DYear, DMonth, DDay, DHour, DTemp);
+	f.close();
 }
 
 
 void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate){
-	string fileName;
-	//This is the way to generate the data vectors in every function.
-	vector<double> year, month, day, time, temp;
-	
-	//Opening The File to be Read.
-	ifstream f(FilePath.c_str());
-	if (f.fail()){
-		cerr<<"Could not open file.\n";
-	}
-	//Extract All Data
-	Reader(f, year, month, day, time, temp);
-	f.close();
 	//Histogram that will contain the data from the csv files.
 	TH1D* hist = new TH1D("Histogram","Temperature On a Day Throughout the Years; Temperature[#circC]; Entries", 300, -20,40);
 	
 	//Pruning the Data
-	for (size_t i= 0;  i<month.size() ; i++){
-		if (month.at(i) == monthToCalculate && day.at(i) == dayToCalculate){
-					hist->Fill(temp.at(i)); 
+	for (size_t i= 0;  i<DMonth.size() ; i++){
+		if (DMonth.at(i) == monthToCalculate && DDay.at(i) == dayToCalculate){
+					hist->Fill(DTemp.at(i)); 
 					i++;
 			}
 		}
 
-	TCanvas* c1 = new TCanvas("c1", "Temperature on a Given Day Throughout the Years", 900, 600);
+	TCanvas* a1 = new TCanvas("a1", "Temperature on a Given Day Throughout the Years", 900, 600);
 	//double mean = hist->GetMean();
 	//double stdev = hist->GetRMS();
 	
@@ -71,31 +65,18 @@ void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate){
 
 	//Draw it and save it
 	hist->Draw();	
-	//Rename filename to fit function and Save canvas.
-	fileName=FileName;
-	fileName.append("_TempOnDay.jpg"); 
-	c1->SaveAs(fileName.c_str());	
+	string fileName=FileName;
+	fileName.append("_TempOnDay.jpg");
+	a1->SaveAs(fileName.c_str());	
 }
 
 void tempTrender::tempPerDay(double Year, double Hour){
-	string fileName;
-	//This is the way to generate the data vectors in every function.
-	vector<double> year, month, day, time, temp;
-	//Opening The File to be Read.
-	ifstream f(FilePath.c_str());
-	if (f.fail()){
-		cerr<<"Could not open file.\n";
-	}
-	//Extract All Data
-	Reader(f, year, month, day, time, temp);
-	f.close();
-	//TempPerDay
 	TH1D* hist = new TH1D("Histogram","Temperature Throughout a Year; Day of the year; Temperature[#circC]", 357, 0, 356);	
 	int bin=0;
-	for (size_t i= 0;  i<time.size() ; i++){
-		if ( year.at(i) == Year && time.at(i) == Hour){
-			hist->SetBinContent(bin, temp.at(i));
-			hist->SetBinError(bin, sqrt(abs(temp.at(i))));
+	for (size_t i= 0;  i<DHour.size() ; i++){
+		if ( DYear.at(i) == Year && DHour.at(i) == Hour){
+			hist->SetBinContent(bin, DTemp.at(i));
+			hist->SetBinError(bin, sqrt(abs(DTemp.at(i))));
 			bin++;
 		}
 	}	
@@ -106,93 +87,77 @@ void tempTrender::tempPerDay(double Year, double Hour){
 	gStyle->SetOptStat(1111);
 	gStyle->SetOptFit(1111);
 	//Draw it and save it
-	hist->Draw();		
-	//Rename filename to fit function and Save canvas.
-	fileName=FileName;
-	fileName.append("_TempThroughoutYear.jpg"); 
+	hist->Draw();	
+	string fileName=FileName;
+	fileName.append("_TempPerDay.jpg");
 	b1->SaveAs(fileName.c_str());
+	
 }
 
 void tempTrender::hotCold(double Hour){
-	
-	//Important Stuff
-	string fileName;
-	vector<double> year, month, day, time, temp;
-	//Opening The File to be Read.
-	ifstream f(FilePath.c_str());
-	if (f.fail()){
-		cerr<<"Could not open file.\n";
-	}
-	//Extract All Data
-	Reader(f, year, month, day, time, temp);
-	f.close();
-	
 	//Plotting Histograms
-	THStack* hs= new THStack("hs"," Hottest and Coldest Days");
-	TCanvas* d1 = new TCanvas("d1", "Hottest and Coldest Days", 900, 600);
+	TCanvas* c1 = new TCanvas("c1", "Hottest and Coldest Days", 900, 600);
 	TH1D* Hot = new TH1D("Hot","Hottest Temperature in a Year; Day of the year; Temperature[#circC]", 357, 0, 356);	
 	TH1D* Cold = new TH1D("Cold","Coldest Temperature in a Year; Day of the year; Temperature[#circC]", 357, 0, 356);
-	
 	// Finding the Hottest and Coldest Days at a Given Hour
-    size_t Year = year.at(0); //Year we start from
-    size_t LastYear =year.at(year.size()-1);
+    size_t Year = DYear.at(0); //Year we start from
+    size_t LastYear =DYear.at(DYear.size()-1);
     int Day=0;
 	for (Year;  Year<=LastYear ; Year++){
 		vector <double> Yrs;
-		for (size_t k=0; k<temp.size(); k++){
-			if (year.at(k) == Year && time.at(k) == Hour){
-			Yrs.push_back(temp.at(k));
+		for (size_t k=0; k<DTemp.size(); k++){
+			if (DYear.at(k) == Year && DHour.at(k) == Hour){
+			Yrs.push_back(DTemp.at(k));
 			Day++;
 			}
 		}
-		Cold->SetBinContent(min_element(Yrs.begin(), Yrs.end())- Yrs.begin(), Day);
-		Hot->SetBinContent(max_element(Yrs.begin(), Yrs.end())- Yrs.begin(), Day);
+		Cold->Fill(min_element(Yrs.begin(), Yrs.end())- Yrs.begin());
+		Hot->Fill(max_element(Yrs.begin(), Yrs.end())- Yrs.begin());
 	}
 	
+	TF1* func = new TF1("Gaussian", Gaussian, 1, 366, 3);
+	func->SetParameters(5, 200, 50); //Starting values for fitting
 	
 	Cold->SetFillColor(kBlue);
-	Hot->SetFillColor(kRed);	
-	hs->Add(Hot);
-	hs->Add(Cold);
-	hs->Draw();
-	//Rename file to fit function.
-	fileName=FileName;
-	fileName.append("_HottestAndColdestDays.jpg"); 
-	d1->SaveAs(fileName.c_str());
+	Hot->SetFillColor(kRed);
+	
+	Hot->Fit(func, "Q0R");
+	cout << "The mean is " << func->GetParameter(1) << endl;
+	cout << "Its uncertainty is " << func->GetParError(1) << endl;
+	TLegend *leg = new TLegend(0.65, 0.75, 0.92, 0.92, "", "NDC");
+	leg->SetFillStyle(0); //Hollow fill (transparent)
+	leg->SetBorderSize(0); //Get rid of the border
+	leg->AddEntry(Hot, "", "F"); //Use object title, draw fill
+	leg->AddEntry(Cold, "A title", "F"); //Use custom title
+	Hot->Draw();
+	Cold->Draw("SAME"); //Draw on top of the existing plot
+	leg->Draw(); //Legends are automatically drawn with "SAME"
+	
+	string fileName=FileName;
+	fileName.append("_HottestandColdestDays.jpg");
+	c1->SaveAs(fileName.c_str());
 	
 }
 
 void tempTrender::Seasons(double Hour){
-	string fileName;
-	//This is the way to generate the data vectors in every function.
-	vector<double> year, month, day, time, temp;
-	
-	//Opening The File to be Read.
-	ifstream f(FilePath.c_str());
-	if (f.fail()){
-		cerr<<"Could not open file.\n";
-	}
-	//Extract All Data
-	Reader(f, year, month, day, time, temp);
-	f.close();
 	//Seasons
-	TCanvas* Seasons = new TCanvas("Seasons","Temperature fluctuations over time.");
+	TCanvas* d1 = new TCanvas("d1","Temperature fluctuations over time.");
 	vector<double> t, TatHr;//time and Temp. at Hour
 	double days;
-	for (size_t i= 0;  i<time.size() ; i++){
-		if ( time.at(i) == Hour){
+	for (size_t i= 0;  i<DHour.size() ; i++){
+		if ( DHour.at(i) == Hour){
 			t.push_back(days);
-			TatHr.push_back(temp.at(i));
+			TatHr.push_back(DTemp.at(i));
 			days++;	
 		}
 	}
+ 
 	TGraph* Graph= new TGraph(days, &t[0], &TatHr[0]);
 	Graph->SetTitle("Temperature over Time; Time (Days);Temperature[#circC]");
-	Graph->Draw();		
-	fileName=FileName;
-	fileName.append("_Seasons.jpg"); 
-	Seasons->SaveAs(fileName.c_str());
-	
+	Graph->Draw();	
+	string fileName=FileName;
+	fileName.append("_Seasons.jpg");
+	d1->SaveAs(fileName.c_str());
 }
 
 
@@ -229,5 +194,7 @@ void tempTrender::split(vector<double> &vect, string &s, string &delimiter){
 			s = s.substr(start);
 		}
 }
-
+double tempTrender::Gaussian(double *x, double *par){
+	return par[0]*exp(-0.5*(x[0]*x[0]-2*x[0]*par[1]+par[1]*par[1])/(par[2]*par[2]));
+}
 
